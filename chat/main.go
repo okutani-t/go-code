cc7eea6e8e7c5835919a8a9b24a5380dcbc6bca6
okutani <okutani.nt@gmail.com>
2016-06-19 17:00:18 +0900

    基本的なチャット完成

--- chat/main.go
+++ chat/main.go
@@ -24,8 +24,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-	// Webサーバーを開始します
+	http.Handle("/room", r)
+	// チャットルームを開始
+	go r.run()
+	// Webサーバーを起動
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
